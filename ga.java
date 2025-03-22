6c474ccbabe2ca974e5c8c4bea1c84f715f66cb4
gaurav-n0419 <neupanegaurav67@gmail.com>
2025-03-22 15:02:05 +0545

    testing java program in runtime version inside vs code !!!!

---
+++ ga.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ga {
+    public static void main(String[] args) {
+        System.out.println("!!! Enter your name !!!");
+        Scanner sc=new Scanner(System.in);
+        String input =sc.nextLine();
+        System.out.println(input);
+        
+
+    }
+    
+}
